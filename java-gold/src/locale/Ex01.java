bc535f6285e3a235d5c7fde24f243ac8579f5994
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-25 16:42:26 +0900

    Localeクラスの宣言方法

--- java-gold/src/locale/Ex01.java
+++ java-gold/src/locale/Ex01.java
@@ -5,18 +5,30 @@ import java.util.Locale;
 public class Ex01 {
 
 	public static void main(String[] args) {
+		
+		/* getDefaultメソッドを使うパターン*/
 		Locale locale = Locale.getDefault();
 		//国の表示
 		System.out.println(locale.getCountry());
 		//言語の表示
 		System.out.println(locale.getLanguage());
 		
+		/*コンストラクターを使うパターン*/
 		Locale locale2 = new Locale("ja", "JP");
 		System.out.println(locale2.getCountry());
 		
+		/*ロケールクラス使うパターン*/
 		Locale locale3 = Locale.US;
 		System.out.println(locale3.getLanguage());
-
+		
+		/*ビルダーを使うパターン*/
+		Locale locale4 = new Locale.Builder()
+							.setLanguage("jp")
+							.setRegion("JP")
+							.build();
+		
+		/*ファクトリメソッドを使うパターン*/
+		Locale locale5 = Locale.forLanguageTag("ex-US-x-lvariant-POSIX");
 	}
 
 }
